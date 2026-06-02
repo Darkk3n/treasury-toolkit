db35c841bccbb8a8fea3f514dd1aa01dce135320
Gerardo Aguilar <gerardo.aguilar@petco.com>
2026-06-02 10:46:04 -0600

    White version of the form

--- FileRenamer/FileRenamerForm.Designer.cs
+++ FileRenamer/FileRenamerForm.Designer.cs
@@ -49,9 +49,12 @@
             // 
             // BtnClean
             // 
-            BtnClean.Location = new Point(196, 11);
+            BtnClean.FlatAppearance.BorderColor = Color.DodgerBlue;
+            BtnClean.FlatStyle = FlatStyle.Flat;
+            BtnClean.Location = new Point(252, 15);
+            BtnClean.Margin = new Padding(4);
             BtnClean.Name = "BtnClean";
-            BtnClean.Size = new Size(75, 23);
+            BtnClean.Size = new Size(96, 40);
             BtnClean.TabIndex = 0;
             BtnClean.Text = "Limpiar";
             BtnClean.UseVisualStyleBackColor = true;
@@ -60,13 +63,17 @@
             // DgvPayments
             // 
             DgvPayments.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DgvPayments.BackgroundColor = Color.White;
+            DgvPayments.BorderStyle = BorderStyle.None;
             DgvPayments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             DgvPayments.Columns.AddRange(new DataGridViewColumn[] { dgvPaymentsColDate, Column1, dgvPaymentsColVendor, dgvPaymentsColConcept, dgvPaymentsColAmount, Column2 });
             DgvPayments.Dock = DockStyle.Bottom;
-            DgvPayments.Location = new Point(0, 98);
+            DgvPayments.EnableHeadersVisualStyles = false;
+            DgvPayments.Location = new Point(0, 138);
+            DgvPayments.Margin = new Padding(4);
             DgvPayments.Name = "DgvPayments";
             DgvPayments.ScrollBars = ScrollBars.Vertical;
-            DgvPayments.Size = new Size(1017, 361);
+            DgvPayments.Size = new Size(1308, 505);
             DgvPayments.TabIndex = 1;
             DgvPayments.CellValidated += DgvPayments_CellValidated;
             DgvPayments.CellValidating += DgvPayments_CellValidating;
@@ -105,9 +112,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(7, 15);
+            label1.Location = new Point(9, 21);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(55, 15);
+            label1.Size = new Size(73, 21);
             label1.TabIndex = 2;
             label1.Text = "Empresa:";
             // 
@@ -116,17 +124,21 @@
             CmbCompany.DropDownStyle = ComboBoxStyle.DropDownList;
             CmbCompany.FormattingEnabled = true;
             CmbCompany.Items.AddRange(new object[] { "--SELECCIONE--", "WOREGG", "BECHEM", "UHLMANN", "NETSTAL", "EMKA" });
-            CmbCompany.Location = new Point(59, 12);
+            CmbCompany.Location = new Point(77, 17);
+            CmbCompany.Margin = new Padding(4);
             CmbCompany.Name = "CmbCompany";
-            CmbCompany.Size = new Size(131, 23);
+            CmbCompany.Size = new Size(167, 29);
             CmbCompany.TabIndex = 3;
             CmbCompany.SelectedIndexChanged += CmbCompany_SelectedIndexChanged;
             // 
             // BtnStart
             // 
-            BtnStart.Location = new Point(277, 12);
+            BtnStart.FlatAppearance.BorderColor = Color.DodgerBlue;
+            BtnStart.FlatStyle = FlatStyle.Flat;
+            BtnStart.Location = new Point(356, 15);
+            BtnStart.Margin = new Padding(4);
             BtnStart.Name = "BtnStart";
-            BtnStart.Size = new Size(75, 23);
+            BtnStart.Size = new Size(101, 40);
             BtnStart.TabIndex = 4;
             BtnStart.Text = "Renombrar";
             BtnStart.UseVisualStyleBackColor = true;
@@ -135,26 +147,32 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 48);
+            label2.BackColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(15, 67);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(128, 15);
+            label2.Size = new Size(166, 21);
             label2.TabIndex = 5;
             label2.Text = "Ubicacion de Archivos:";
             // 
             // LblFolder
             // 
             LblFolder.AutoSize = true;
-            LblFolder.Location = new Point(181, 48);
+            LblFolder.Location = new Point(233, 67);
+            LblFolder.Margin = new Padding(4, 0, 4, 0);
             LblFolder.Name = "LblFolder";
-            LblFolder.Size = new Size(16, 15);
+            LblFolder.Size = new Size(19, 21);
             LblFolder.TabIndex = 6;
             LblFolder.Text = "...";
             // 
             // BtnFileDialog
             // 
-            BtnFileDialog.Location = new Point(146, 42);
+            BtnFileDialog.FlatAppearance.BorderColor = Color.DodgerBlue;
+            BtnFileDialog.FlatStyle = FlatStyle.Flat;
+            BtnFileDialog.Location = new Point(188, 59);
+            BtnFileDialog.Margin = new Padding(4);
             BtnFileDialog.Name = "BtnFileDialog";
-            BtnFileDialog.Size = new Size(29, 26);
+            BtnFileDialog.Size = new Size(37, 36);
             BtnFileDialog.TabIndex = 7;
             BtnFileDialog.Text = "...";
             BtnFileDialog.UseVisualStyleBackColor = true;
@@ -163,27 +181,31 @@
             // LblConsecutive
             // 
             LblConsecutive.AutoSize = true;
-            LblConsecutive.Location = new Point(362, 16);
+            LblConsecutive.Location = new Point(465, 22);
+            LblConsecutive.Margin = new Padding(4, 0, 4, 0);
             LblConsecutive.Name = "LblConsecutive";
-            LblConsecutive.Size = new Size(76, 15);
+            LblConsecutive.Size = new Size(98, 21);
             LblConsecutive.TabIndex = 8;
             LblConsecutive.Text = "Consecutivo:";
             LblConsecutive.Visible = false;
             // 
             // TxtConsecutive
             // 
-            TxtConsecutive.Location = new Point(444, 11);
+            TxtConsecutive.BorderStyle = BorderStyle.FixedSingle;
+            TxtConsecutive.Location = new Point(571, 18);
+            TxtConsecutive.Margin = new Padding(4);
             TxtConsecutive.Name = "TxtConsecutive";
-            TxtConsecutive.Size = new Size(119, 23);
+            TxtConsecutive.Size = new Size(152, 29);
             TxtConsecutive.TabIndex = 9;
             TxtConsecutive.Visible = false;
             TxtConsecutive.KeyPress += TxtConsecutive_KeyPress;
             // 
             // FileRenamerForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1017, 459);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(1308, 643);
             Controls.Add(TxtConsecutive);
             Controls.Add(LblConsecutive);
             Controls.Add(BtnFileDialog);
@@ -194,6 +216,10 @@
             Controls.Add(label1);
             Controls.Add(DgvPayments);
             Controls.Add(BtnClean);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4);
+            MaximizeBox = false;
             Name = "FileRenamerForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tesoreria - Renombrador de Archivos";
